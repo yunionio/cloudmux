7f3a871e839514c21dfaf53001def000d977fc7c
Qu Xuan <quxuan@yunionyun.com>
2021-02-22 15:30:04 +0800

    feat(region): nat op support

---
+++ pkg/apis/compute/nat_skus.go
@@ -0,0 +1,40 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compute
+
+import "yunion.io/x/onecloud/pkg/apis"
+
+const (
+	NAT_SKU_AVAILABLE = "available"
+	NAT_SKU_SOLDOUT   = "soldout"
+
+	ALIYUN_NAT_SKU_DEFAULT = "Default"
+)
+
+type NatSkuListInput struct {
+	apis.EnabledStatusStandaloneResourceListInput
+	apis.ExternalizedResourceBaseListInput
+
+	RegionalFilterListInput
+
+	PostpaidStatus string `json:"postpaid_stauts"`
+	PrepaidStatus  string `json:"prepaid_status"`
+}
+
+type NatSkuDetails struct {
+	apis.EnabledStatusStandaloneResourceDetails
+
+	CloudregionResourceInfo
+}
