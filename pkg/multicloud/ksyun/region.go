53ff8bae72ca40f2a0e9464f4a6e2e3e7cfd9cf8
ioito <qu_xuan@icloud.com>
2023-08-07 15:36:51 +0800

    fix(ksyun): add region code

--- pkg/multicloud/ksyun/region.go
+++ pkg/multicloud/ksyun/region.go
@@ -14,10 +14,116 @@
 
 package ksyun
 
+import (
+	"fmt"
+
+	api "yunion.io/x/cloudmux/pkg/apis/compute"
+	"yunion.io/x/cloudmux/pkg/cloudprovider"
+	"yunion.io/x/cloudmux/pkg/multicloud"
+)
+
 type SRegion struct {
+	multicloud.SRegion
+	multicloud.SNoObjectStorageRegion
+	multicloud.SNoLbRegion
 	client *SKsyunClient
+
+	Region     string
+	RegionName string
+}
+
+func (self *SRegion) GetId() string {
+	return self.Region
+}
+
+func (self *SRegion) GetGlobalId() string {
+	return fmt.Sprintf("%s/%s", api.CLOUD_PROVIDER_KSYUN, self.Region)
+}
+
+func (self *SRegion) GetProvider() string {
+	return api.CLOUD_PROVIDER_KSYUN
+}
+
+func (self *SRegion) GetCloudEnv() string {
+	return api.CLOUD_PROVIDER_KSYUN
+}
+
+func (self *SRegion) GetGeographicInfo() cloudprovider.SGeographicInfo {
+	geo, ok := map[string]cloudprovider.SGeographicInfo{
+		"cn-northwest-1": api.RegionQingYang,
+		"ap-singapore-1": api.RegionSingapore,
+		"cn-beijing-6":   api.RegionBeijing,
+		"cn-guangzhou-1": api.RegionGuangzhou,
+		"cn-shanghai-2":  api.RegionShanghai,
+	}[self.Region]
+	if ok {
+		return geo
+	}
+	return cloudprovider.SGeographicInfo{}
+}
+
+func (self *SRegion) GetName() string {
+	return self.RegionName
+}
+
+func (self *SRegion) GetI18n() cloudprovider.SModelI18nTable {
+	table := cloudprovider.SModelI18nTable{}
+	table["name"] = cloudprovider.NewSModelI18nEntry(self.GetName()).CN(self.GetName()).EN(self.Region)
+	return table
+}
+
+func (self *SRegion) GetStatus() string {
+	return api.CLOUD_REGION_STATUS_INSERVER
 }
 
 func (self *SRegion) GetClient() *SKsyunClient {
 	return self.client
+}
+
+func (self *SRegion) CreateEIP(opts *cloudprovider.SEip) (cloudprovider.ICloudEIP, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (region *SRegion) CreateISecurityGroup(conf *cloudprovider.SecurityGroupCreateInput) (cloudprovider.ICloudSecurityGroup, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (region *SRegion) GetISecurityGroupById(secgroupId string) (cloudprovider.ICloudSecurityGroup, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (region *SRegion) GetISecurityGroupByName(opts *cloudprovider.SecurityGroupFilterOptions) (cloudprovider.ICloudSecurityGroup, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (self *SRegion) CreateIVpc(opts *cloudprovider.VpcCreateOptions) (cloudprovider.ICloudVpc, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (self *SRegion) GetIVpcs() ([]cloudprovider.ICloudVpc, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (self *SRegion) GetIVpcById(id string) (cloudprovider.ICloudVpc, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (region *SRegion) GetCapabilities() []string {
+	return region.client.GetCapabilities()
+}
+
+func (self *SRegion) GetIEipById(eipId string) (cloudprovider.ICloudEIP, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (self *SRegion) GetIEips() ([]cloudprovider.ICloudEIP, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (self *SRegion) GetIZones() ([]cloudprovider.ICloudZone, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (self *SRegion) GetIZoneById(id string) (cloudprovider.ICloudZone, error) {
+	return nil, cloudprovider.ErrNotImplemented
 }
