100b6207c544bd4e09dfe88069dcef2f8b7336f9
ioito <qu_xuan@icloud.com>
2023-09-19 15:33:38 +0800

    fix(aws): storage emulated

--- pkg/multicloud/aws/storage.go
+++ pkg/multicloud/aws/storage.go
@@ -78,6 +78,10 @@ func (self *SStorage) GetIStoragecache() cloudprovider.ICloudStoragecache {
 	return self.zone.region.getStoragecache()
 }
 
+func (self *SStorage) IsEmulated() bool {
+	return true
+}
+
 func (self *SStorage) GetIZone() cloudprovider.ICloudZone {
 	return self.zone
 }
