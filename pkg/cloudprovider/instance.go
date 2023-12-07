accfcd734d30deb8a1194f8e2ad2f6aa753d5219
ioito <qu_xuan@icloud.com>
2023-12-07 15:57:29 +0800

    fix: add bios options for create vm

--- pkg/cloudprovider/instance.go
+++ pkg/cloudprovider/instance.go
@@ -143,6 +143,7 @@ type SManagedVMCreateConfig struct {
 	OsType              string
 	OsDistribution      string
 	OsVersion           string
+	Bios                string
 	InstanceType        string // InstanceType 不为空时，直接采用InstanceType创建机器。
 	Cpu                 int
 	MemoryMB            int
