4de0808a398fcb98e379cf170f24f98fe1503cc2
马鸿飞 <mahongfei@yunion.cn>
2024-02-28 11:01:45 +0800

    feat(ksyun): support ecs list

---
+++ pkg/multicloud/ksyun/shell/disks.go
@@ -0,0 +1,50 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shell
+
+import (
+	"yunion.io/x/cloudmux/pkg/multicloud/ksyun"
+
+	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/shellutils"
+)
+
+func init() {
+	type DiskListOptions struct {
+		DiskId      []string
+		StorageType string
+		ZoneId      string
+	}
+	shellutils.R(&DiskListOptions{}, "disk-list", "list disks", func(cli *ksyun.SRegion, args *DiskListOptions) error {
+		res, err := cli.GetDisks(args.DiskId, args.StorageType, args.ZoneId)
+		if err != nil {
+			return errors.Wrap(err, "GetDisks")
+		}
+		printList(res, 0, 0, 0, []string{})
+		return nil
+	})
+
+	type InstanceDiskListOptions struct {
+		INSTANCE_ID string
+	}
+	shellutils.R(&InstanceDiskListOptions{}, "instance-disk-list", "list disks", func(cli *ksyun.SRegion, args *InstanceDiskListOptions) error {
+		res, err := cli.GetDiskByInstanceId(args.INSTANCE_ID)
+		if err != nil {
+			return errors.Wrap(err, "GetDiskByInstanceId")
+		}
+		printList(res, 0, 0, 0, []string{})
+		return nil
+	})
+}
