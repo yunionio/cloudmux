81cb5deff690a14fa91119107ca414d799a433bc
Qu Xuan <qu_xuan@icloud.com>
2025-11-21 18:54:55 +0800

    fix(esxi): support resource pool

--- pkg/multicloud/esxi/shell/cluster.go
+++ pkg/multicloud/esxi/shell/cluster.go
@@ -15,7 +15,6 @@
 package shell
 
 import (
-	"yunion.io/x/pkg/errors"
 	"yunion.io/x/pkg/util/shellutils"
 
 	"yunion.io/x/cloudmux/pkg/multicloud/esxi"
@@ -35,51 +34,6 @@ func init() {
 			return err
 		}
 		printList(clusters, nil)
-		return nil
-	})
-
-	type ClusterPoolListOptions struct {
-		DATACENTER string `help:"List clusters in datacenter"`
-		CLUSTER    string `help:"List cluster resource pool"`
-	}
-
-	shellutils.R(&ClusterPoolListOptions{}, "cluster-pool-list", "List all cluster resource pool", func(cli *esxi.SESXiClient, args *ClusterPoolListOptions) error {
-		dc, err := cli.FindDatacenterByMoId(args.DATACENTER)
-		if err != nil {
-			return err
-		}
-		cluster, err := dc.GetCluster(args.CLUSTER)
-		if err != nil {
-			return err
-		}
-		pools, err := cluster.ListResourcePools()
-		if err != nil {
-			return errors.Wrap(err, "ListResourcePools")
-		}
-		printList(pools, nil)
-		return nil
-	})
-
-	type ClusterPoolSyncOptions struct {
-		DATACENTER string `help:"List clusters in datacenter"`
-		CLUSTER    string `help:"List cluster resource pool"`
-		Name       string `help:"Resource pool name"`
-	}
-
-	shellutils.R(&ClusterPoolSyncOptions{}, "cluster-pool-sync", "Sync cluster resource pool", func(cli *esxi.SESXiClient, args *ClusterPoolSyncOptions) error {
-		dc, err := cli.FindDatacenterByMoId(args.DATACENTER)
-		if err != nil {
-			return err
-		}
-		cluster, err := dc.GetCluster(args.CLUSTER)
-		if err != nil {
-			return err
-		}
-		pool, err := cluster.SyncResourcePool(args.Name)
-		if err != nil {
-			return errors.Wrap(err, "SyncResourcePool")
-		}
-		printObject(pool)
 		return nil
 	})
 
--- pkg/multicloud/esxi/shell/resourcepool.go
+++ pkg/multicloud/esxi/shell/resourcepool.go
@@ -15,7 +15,6 @@
 package shell
 
 import (
-	"yunion.io/x/pkg/errors"
 	"yunion.io/x/pkg/util/shellutils"
 
 	"yunion.io/x/cloudmux/pkg/multicloud/esxi"
@@ -38,26 +37,4 @@ func init() {
 		return nil
 	})
 
-	type ResourcePoolCreateOptions struct {
-		DATACENTER string `help:"Create resource pool in datacenter"`
-		CLUSTER    string `help:"Cluster name"`
-		NAME       string `help:"Resource pool name"`
-	}
-
-	shellutils.R(&ResourcePoolCreateOptions{}, "resource-pool-create", "Create resource pool", func(cli *esxi.SESXiClient, args *ResourcePoolCreateOptions) error {
-		dc, err := cli.FindDatacenterByMoId(args.DATACENTER)
-		if err != nil {
-			return err
-		}
-		cluster, err := dc.GetCluster(args.CLUSTER)
-		if err != nil {
-			return errors.Wrap(err, "GetCluster")
-		}
-		pool, err := cluster.CreateResourcePool(args.NAME)
-		if err != nil {
-			return err
-		}
-		printObject(pool)
-		return nil
-	})
 }
