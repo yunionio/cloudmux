e602ac99af3cf991fb66fad1797b3a4953883411
Qu Xuan <qu_xuan@icloud.com>
2026-07-16 19:24:32 +0800

    fix(esxi): format metric-list shell params

--- pkg/multicloud/azure/shell/monitor.go
+++ pkg/multicloud/azure/shell/monitor.go
@@ -15,6 +15,8 @@
 package shell
 
 import (
+	"time"
+
 	"yunion.io/x/log"
 	"yunion.io/x/pkg/util/shellutils"
 
@@ -26,6 +28,12 @@ func init() {
 	type LoadbalancerListOptions struct {
 	}
 	shellutils.R(&cloudprovider.MetricListOptions{}, "metric-list", "List metrics", func(cli *azure.SRegion, args *cloudprovider.MetricListOptions) error {
+		if args.StartTime.IsZero() {
+			args.StartTime = time.Now().Add(-time.Hour)
+		}
+		if args.EndTime.IsZero() {
+			args.EndTime = time.Now()
+		}
 		metrics, err := cli.GetClient().GetMetrics(args)
 		if err != nil {
 			return err
